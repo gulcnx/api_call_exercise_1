3809c594e420edf10c5cc9791a723bfef4eef032
gülçin çetin <cgulcn777@gmail.com>
2024-11-19 17:27:16 +0300

    initial commit

---
+++ api_call_exercise/api_call_exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_call_exerciseApp.swift
+//  api_call_exercise
+//
+//  Created by gülçin çetin on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct api_call_exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ api_call_exerciseTests/api_call_exerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  api_call_exerciseTests.swift
+//  api_call_exerciseTests
+//
+//  Created by gülçin çetin on 18.11.2024.
+//
+
+import Testing
+@testable import api_call_exercise
+
+struct api_call_exerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
